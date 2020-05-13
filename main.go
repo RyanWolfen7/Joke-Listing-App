e1e2ad181a83c55a10223d78d7027c295d5b6045
RyanWolfen7 <wolfen7lada@gmail.com>
2020-05-13 00:01:31 -0500

    set up mock data and updated endpoint functions

--- main.go
+++ main.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
-// Useful Gin Fucntions
-// gin.c = context
-// gin.H = map[string]interface{} see https://gobyexample.com/json
+/* Useful Go
+strconv = converst strings to basic data types see https://golang.org/pkg/strconv/
+			- Atoi = string to int
+			- Itoa = int to string
+*/
+
+/* Useful Gin Fucntions
+gin.c = context
+gin.H = map[string]interface{} see https://gobyexample.com/json
+*/
+
+// Joke contains information about a single Joke
+type Joke struct {
+	ID    int    `json:"id" binding:"required"`
+	Likes int    `json:"likes"`
+	Joke  string `json:"joke" binding:"required"`
+}
+
+// jokes temp joke data
+var jokes = []Joke{
+	Joke{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
+	Joke{2, 0, "What do you call a fake noodle? An Impasta."},
+	Joke{3, 0, "How many apples grow on a tree? All of them."},
+	Joke{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
+	Joke{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
+	Joke{6, 0, "Why did the coffee file a police report? It got mugged."},
+	Joke{7, 0, "How does a penguin build it's house? Igloos it together."},
+}
 
 func main() {
 	// default gin router
@@ -38,15 +64,25 @@ func main() {
 // JokeHandler retieves list of available jokes
 func JokeHandler(context *gin.Context) {
 	context.Header("Content-Type", "application/json")
-	context.JSON(http.StatusOK, gin.H{
-		"message": "Jokes handler not implemented yet",
-	})
+	context.JSON(http.StatusOK, jokes)
 }
 
 // LikeJoke increments the likes of a particular joke Item
 func LikeJoke(context *gin.Context) {
-	context.Header("Content-Type", "application/json")
-	context.JSON(http.StatusOK, gin.H{
-		"message": "LikeJoke handler not implemented yet",
-	})
+	//confirm JOKE ID is valid
+	if jokeid, err := strconv.Atoi(context.Param("jokeID")); err == nil {
+		// find joke, and increment likes
+		for i := 0; i < len(jokes); i++ {
+			if jokes[i].ID == jokeid {
+				jokes[i].Likes++
+			}
+		}
+
+		// return a pointer to the updated jokes list
+		context.JSON(http.StatusOK, &jokes)
+	} else {
+		// ID invalid
+		context.AbortWithStatus(http.StatusNotFound)
+	}
+
 }
